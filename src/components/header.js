378073aebc9fd0c7c6d02d0e5ae58d01b5706c50
cattanisam <samantha.cattani@mail.mcgill.ca>
2019-08-03 17:26:26 -0400

    please

--- src/components/header.js
+++ src/components/header.js
@@ -12,11 +12,28 @@ const Header = ({ siteTitle }) => (
   <header style = {{backgroundImage: `url(${Background})`, marginBottom: `1.45rem`}}>
       <img src={Logo} style={{height: 400, position: 'relative', left: '50%',
         transform: 'translate(-50%, 0)'}} alt="" />
-      <h1 style= {{position: 'relative', left: `44%`, transform: 'translate(0, -50%)', color: `white`}} alt="">Frosh 3019</h1>
-      <h2 style= {{position: 'relative', left: `40%`, transform: 'translate(0, -50%)', color: `white`}} alt="">The Frosh of Tomorrow</h2>
+        <div style = {{margin: `0 auto`,
+        justifyContent: `center`,
+        alignItems:`center`,
+        height:`13vh`,
+        display: `flex`}}>
+      <h1 style= {{color: `white`}}>Frosh 3019</h1>
+      </div>
+      <div style = {{margin: `0 auto`,
+        justifyContent: `center`,
+        alignItems:`center`,
+        height:`13vh`,
+        display: `flex`}}>
+      <h2 style= {{color: `white`}}>The Frosh of Tomorrow</h2>
+      </div>
+      <div style = {{margin: `0 auto`,
+        justifyContent: `center`,
+        alignItems:`center`,
+        height:`13vh`,
+        display: `flex`}}>
       <Dropdown id={`hi`}>
-  <Dropdown.Toggle variant="success" id="dropdown-basic">
-    Dropdown Button
+  <Dropdown.Toggle variant="success" id="dropdown-basic" style = {{width: 500}}>
+    Menu
   </Dropdown.Toggle>
 
   <Dropdown.Menu>
@@ -25,6 +42,7 @@ const Header = ({ siteTitle }) => (
     <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
   </Dropdown.Menu>
 </Dropdown>
+</div>
     </header>
     </span>
 )
