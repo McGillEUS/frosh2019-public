79c8167550170f98c7f919d876e942b7eb93b620
cattanisam <samantha.cattani@mail.mcgill.ca>
2019-08-03 17:11:26 -0400

    please

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,7 @@ import React from "react"
 import Logo from "../images/Frosh3019Logo.png"
 import Background from "../images/HeaderBackground.png"
 import '../pages/App.css'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
 const Header = ({ siteTitle }) => (
   
@@ -11,8 +12,9 @@ const Header = ({ siteTitle }) => (
   <header style = {{backgroundImage: `url(${Background})`, marginBottom: `1.45rem`}}>
       <img src={Logo} style={{height: 400, position: 'relative', left: '50%',
         transform: 'translate(-50%, 0)'}} alt="" />
-      <h1 style= {{position:`relative`, color: `white`}}>Frosh 3019</h1>
-      <h2 style= {{position:`relative`, color: `white`}}>The Frosh of Tomorrow</h2>
+      <h1 style= {{position: 'relative', left: `44%`, transform: 'translate(0, -50%)', color: `white`}} alt="">Frosh 3019</h1>
+      <h2 style= {{position: 'relative', left: `40%`, transform: 'translate(0, -50%)', color: `white`}} alt="">The Frosh of Tomorrow</h2>
+      <DropdownButton/>
     </header>
     </span>
 )
