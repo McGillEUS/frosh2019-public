cc773c9b59b33db53c1e4f3b6784b0673c2661f7
cattanisam <samantha.cattani@mail.mcgill.ca>
2019-08-04 11:54:42 -0400

    some styling

--- src/pages/bursary.js
+++ src/pages/bursary.js
@@ -5,8 +5,6 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Bursary = () => (
-  <Layout>
-      <SEO title="Bursary Application" />
-      </Layout>
+    <embed src="files/Bursary.pdf" type="application/pdf" width="100%" height="600px" />
 )
 export default Bursary
